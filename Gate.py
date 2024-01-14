fbfcf03c5a04b1a125093802ad3da87e62618726
Constantin TEJA <constantin.teja@stud.acs.upb.ro>
2024-01-15 01:13:40 +0200

    Refactor and added new functionalities

---
+++ Gate.py
@@ -0,0 +1,47 @@
+from Quantum_Helper import Quantum_Helper as qh
+import numpy as np
+
+class Gate:
+
+    gates = {}
+
+    def __init__(self, gate):
+        # Assert shape
+        shape = np.shape(gate)
+        assert len(shape) == 2
+        assert shape[0] == shape[1]
+        assert shape[0] > 1
+
+        # Assert to be unitary
+        assert Gate.__is_valid(gate) == True
+
+        # Assert dimension
+        n_qubits = np.log2(len(gate))
+        assert n_qubits > 0
+        assert round(n_qubits) == n_qubits
+
+        # Save info to object
+        self.__n_qubits = n_qubits
+        self.__gate = gate
+
+    @staticmethod
+    def __is_valid(gate):
+        return np.allclose(np.dot(gate, qh.compute_adjoint(gate)), np.identity(len(gate)))
+        
+    # cteja TODO: delete
+    def get_gate(self):
+        return self.__gate
+
+    @staticmethod
+    def compute_standard_gates():
+        Gate.gates["H"] = Gate(np.array([[1, 1], [1, -1]]) / qh.sqrt2)
+        Gate.gates["X"] = Gate(np.array([[0, 1], [1, 0]]))
+        Gate.gates["S"] = Gate(np.array([[1, 0], [0, 1j]]))
+        Gate.gates["CNOT"] = Gate(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]))
+        #Gate.gates["CH"] = Gate(np.array([[1, 0, 0, 0], [0, qh.sqrt2, 0, qh.sqrt2], [0, 0, 1, 0], [0, qh.sqrt2, 0, qh.sqrt2]])) #TODO
+        Gate.gates["SWAP"] = Gate(np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0 ,0], [0, 0, 0, 1]]))
+        Gate.gates["CNOT10"] = Gate(np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1 ,0], [0, 0, 0, 1]]))
+        Gate.gates["TOFFOLI"] = Gate(np.array([[1, 0, 0, 0, 0, 0, 0, 0], [0, 1 ,0 ,0 ,0 ,0 ,0 ,0], [0, 0, 1, 0, 0, 0, 0, 0], [0 ,0 ,0 ,1 ,0 ,0 ,0 ,0], [0 ,0 ,0 ,0 ,1 ,0 ,0 ,0], [0 ,0 ,0 ,0 ,0 ,1 ,0 ,0], [0 ,0 ,0 ,0 ,0 ,0 ,0 ,1], [0 ,0 ,0 ,0 ,0 ,0 ,1 ,0]]))
+
+# cteja TODO: move in quantum simulator file
+Gate.compute_standard_gates()
---
+++ tests.py
@@ -0,0 +1,82 @@
+import unittest
+import numpy as np
+
+from Gate import Gate
+from State import State
+from Quantum_Helper import Quantum_Helper as qh
+
+class Gate_Tests(unittest.TestCase):
+        
+    def test_create_gate_success(self):
+        self.assertTrue(np.array_equal(Gate(np.identity(2)).get_gate(), np.identity(2)))
+        self.assertTrue(np.array_equal(Gate(np.identity(4)).get_gate(), np.identity(4)))
+        self.assertTrue(np.array_equal(Gate(np.identity(8)).get_gate(), np.identity(8)))
+        self.assertTrue(np.allclose(Gate(np.array([[1, 1], [1, -1]]) / np.sqrt(2)).get_gate(), Gate.gates["H"].get_gate()))
+
+    def test_create_gate_dimension(self):
+        with self.assertRaises(AssertionError): Gate(1)
+        with self.assertRaises(AssertionError): Gate(np.array([1]))
+
+    def test_create_gate_shape(self):
+        with self.assertRaises(AssertionError): Gate([1, 2, 3])
+        with self.assertRaises(AssertionError): Gate(np.array([[1, 0], [1, 0], [1, 0]]))
+        
+    def test_create_gate_unitary(self):
+        with self.assertRaises(AssertionError): Gate(np.array([[1, 1], [1, 1]]))
+
+    def test_create_gate_tensor_result(self):
+        with self.assertRaises(AssertionError): Gate(np.identity(3))
+
+
+class State_Tests(unittest.TestCase):
+
+    def test_create_state(self):
+        self.assertTrue(np.array_equal(State(2).state, np.array([[1], [0], [0], [0]])))
+        self.assertTrue(np.array_equal(State(3).state, np.array([[1], [0], [0], [0], [0], [0], [0], [0]])))
+
+    def test_initialize_state_success(self):
+        state_1 = State(2)
+        state_2 = State(3)
+
+        state_1.initialize_state(np.array([1, 1]))
+        state_2.initialize_state(np.array([1, 1, 1]))
+
+        self.assertTrue(np.array_equal(state_1.state, np.array([[0], [0], [0], [1]])))
+        self.assertTrue(np.array_equal(state_2.state, np.array([[0], [0], [0], [0], [0], [0], [0], [1]])))
+
+    def test_intialize_state_fail(self):
+        with self.assertRaises(AssertionError): State(3).initialize_state(np.array([1, 1]))
+
+    def test_apply_H_gate(self):
+        state = State(2)
+        state.apply_H_gate(0)
+        state.apply_H_gate(1)
+
+        self.assertTrue(np.allclose(state.state, np.array([[0.5], [0.5], [0.5], [0.5]])))
+
+    def test_apply_X_gate(self):
+        state = State(1)
+        state.apply_X_gate(0)
+
+        self.assertTrue(np.allclose(state.state, np.array([[0], [1]])))
+
+
+class Quantum_Helper_Tests(unittest.TestCase):
+    
+    def test_measurement(self):
+        state_1 = State(1)
+        state_2 = State(2)
+        state_3 = State(1)
+
+        state_2.initialize_state(np.array([1, 1]))
+        state_3.initialize_state([1])
+
+        self.assertTrue(np.allclose(qh.measure(state_1.state, "COMPUTATIONAL"), np.array([[1], [0]])))
+        self.assertTrue(np.allclose(qh.measure(state_2.state, "COMPUTATIONAL"), np.array([[0], [0], [0], [1]])))
+        self.assertTrue(np.allclose(qh.measure(state_3.state, "HADAMARD"), np.array([[1], [1]]) / qh.sqrt2))
+
+
+
+# Main entry point to run the tests
+if __name__ == '__main__':
+    unittest.main()
